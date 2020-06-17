0286527f9a6487a511f6d1cd24a17272acd0f3f4
unknown <Damasio@FDN-LT-038099.dvuadmin.net>
2020-06-16 21:26:57 -0300

    curso completo

--- js/introducao-javascript/js/buscar-pacientes.js
+++ js/introducao-javascript/js/buscar-pacientes.js
@@ -1,12 +1,25 @@
 var botaoAdicionar = document.querySelector("#buscar-pacientes");
 
-botaoAdicionar.addEventListener("click", function(){
-	console.log("Buscando Pacientes ...");
+botaoAdicionar.addEventListener("click", function() {
+    var xhr = new XMLHttpRequest();
 
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
-	xhr.addEventListener("load", function(){
-		console.log(xhr.responseText);
-	});
-	xhr.send();
+    xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
+
+    xhr.addEventListener("load", function() {
+        var erroAjax = document.querySelector("#erro-ajax");
+
+        if (xhr.status == 200) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
+
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            erroAjax.classList.remove("invisivel");
+        }
+    });
+
+    xhr.send();
 });
--- js/introducao-javascript/js/calcula-imc.js
+++ js/introducao-javascript/js/calcula-imc.js
@@ -1,48 +1,64 @@
-var mudarTitulo = document.querySelector(".titulo");
-mudarTitulo.textContent = "Aparecida Nutricionista";
-
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
 
+for (var i = 0; i < pacientes.length; i++) {
 
+    var paciente = pacientes[i];
 
-for(var i = 0; i < pacientes.length; i++ ){
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-	var paciente = pacientes[i];
-	
-	var tdPeso = paciente.querySelector(".info-peso");
-	var peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-	var tdAltura = paciente.querySelector(".info-altura");
-	var altura = tdAltura.textContent;
+    var tdImc = paciente.querySelector(".info-imc");
 
-	var tdImc = paciente.querySelector(".info-imc");
+    var pesoEhValido = validaPeso(peso);
+    var alturaEhValida = validaAltura(altura);
 
-	var imc = peso / (altura * altura);
+    if (!pesoEhValido) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
 
-	var pesoEhValido = true;
-	var alturaEhValido = true;
+    if (!alturaEhValida) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
 
-	if (peso <= 0 || peso >=400 ) {
-		tdImc.textContent = "Peso inválido";
-		pesoEhValido = false;
-		paciente.classList.add("paciente-invalido");
-		
-	}
-
-	if (altura <=0 || altura >=3.00){
-		tdImc.textContent = "Altura inválida";
-		alturaEhValido = false;
-		paciente.classList.add("paciente-invalido");
-	}
-
-	if(alturaEhValido && pesoEhValido){
-		tdImc.textContent = imc.toFixed(2);
-	}
+    if (pesoEhValido && alturaEhValida) {
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
 }
 
-function calculaImc(peso, altura){
-	var imc = 0;
-	imc = peso / (altura * altura);
-	return imc.toFixed(2);
+function calculaImc(peso, altura) {
+    var imc = 0;
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+
+    if (peso >= 0 && peso <= 1000) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura) {
+
+    if (altura >= 0 && altura <= 3.00) {
+        return true;
+    } else {
+        return false;
+    }
 }
